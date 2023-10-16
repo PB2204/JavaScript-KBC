f1f2d1bdc2bc17f32c13a8b148fbee2826acebc6
Mohamed Jubair <mohamed.jubair@livspace.com>
2023-10-16 18:29:40 +0530

    Refactor questions to load dynamically

---
+++ questions/javascript.js
@@ -0,0 +1,144 @@
+const data = [
+    {
+        name: 'Brendan Eich',
+        type: 'list',
+        message: 'Who is often referred to as the "father of JavaScript"?\n',
+        choices: [
+            'Brendan Eich',
+            'Mark Zuckerberg',
+            'Tim Berners-Lee',
+            'Bill Gates',
+        ],
+    },
+    {
+        name: 'Enhancing user interfaces',
+        type: 'list',
+        message: 'What is the primary purpose of JavaScript in web development?\n',
+        choices: [
+            'Styling web pages',
+            'Creating databases',
+            'Enhancing user interfaces',
+            'Sending emails',
+        ],
+    },
+    {
+        name: 'const',
+        type: 'list',
+        message: 'Which keyword is used to declare a constant variable in JavaScript?\n',
+        choices: ['var', 'let', 'const', 'fixed'],
+    },
+    {
+        name: 'Character',
+        type: 'list',
+        message: 'Which of the following is not a valid JavaScript data type?\n',
+        choices: [
+            'Number',
+            'String',
+            'Character',
+            'Boolean',
+        ],
+    },
+    {
+        name: 'Document Object Model',
+        type: 'list',
+        message: 'What does the "DOM" stand for in web development?\n',
+        choices: [
+            'Document Object Model',
+            'Data Output Mechanism',
+            'Digital Object Manager',
+            'Document Orientation Module',
+        ],
+    },
+    {
+        name: 'var',
+        type: 'list',
+        message: 'Which keyword is used to declare a variable in JavaScript?\n',
+        choices: ['var', 'let', 'const', 'variable'],
+    },
+    {
+        name: 'Document Object Model',
+        type: 'list',
+        message: 'What does the acronym "DOM" stand for in web development?\n',
+        choices: [
+            'Document Object Model',
+            'Data Object Model',
+            'Design Object Model',
+            'Document Object Method',
+        ],
+    },
+    {
+        name: 'pop()',
+        type: 'list',
+        message: 'Which built-in method removes the last element from an array and returns that element in JavaScript?\n',
+        choices: ['pop()', 'shift()', 'splice()', 'push()'],
+    },
+    {
+        name: 'To execute a function when an event occurs',
+        type: 'list',
+        message: 'What is the purpose of the `addEventListener` method in JavaScript?\n',
+        choices: [
+            'To add a class to an element',
+            'To execute a function when an event occurs',
+            'To create a new DOM element',
+            'To change the CSS style of an element',
+        ],
+    },
+    {
+        name: '===',
+        type: 'list',
+        message: 'Which operator is used for strict equality in JavaScript?\n',
+        choices: ['==', '===', '=', '!='],
+    },
+    {
+        name: 'To transform elements in an array',
+        type: 'list',
+        message: 'What is the purpose of the JavaScript `map()` function?\n',
+        choices: [
+            'To filter elements in an array',
+            'To transform elements in an array',
+            'To remove elements from an array',
+            'To find the maximum value in an array',
+        ],
+    },
+    {
+        name: 'The number is converted to a string and concatenated',
+        type: 'list',
+        message: 'In JavaScript, what is the result of adding a number and a string?\n',
+        choices: [
+            'An error',
+            'The number is converted to a string and concatenated',
+            'The string is converted to a number and added',
+            'A random value is generated',
+        ],
+    },
+    {
+        name: 'pop()',
+        type: 'list',
+        message: 'Which method is used to remove the last element from an array and return it?\n',
+        choices: ['pop()', 'shift()', 'splice()', 'push()'],
+    },
+    {
+        name: 'Not a Number',
+        type: 'list',
+        message: 'In JavaScript, what does the `NaN` value represent?\n',
+        choices: [
+            'Not a Number',
+            'Zero',
+            'Infinity',
+            'Undefined',
+        ],
+    },
+    {
+        name: 'To execute a function for each element in an array',
+        type: 'list',
+        message: 'What is the purpose of the JavaScript `forEach()` method?\n',
+        choices: [
+            'To execute a function for each element in an array',
+            'To create a new array with filtered elements',
+            'To find the first element that matches a condition',
+            'To sort the elements in an array',
+        ],
+    }
+]
+
+export default data;
